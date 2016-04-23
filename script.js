e9783ac79e57f4c3c1f4b51607b1ff1ec3ef32ea
aashutoshjha <ashutosh.jha1991@gmail.com>
2016-04-23 13:37:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,17 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Bhaiyaji!')
+            return bot.say('By the time my creator responds, do you want me to tell you a joke? I am their pet bot, "bhot"')
                 .then(() => 'askName');
         }
     },
 
     askName: {
-        prompt: (bot) => bot.say('What\'s your name? How would you like me to help you?'),
+        //prompt: (bot) => bot.say('What\'s your name? How would you like me to help you?'),
         receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
+            const response = message.text;
+            if response == "yes"
+            //return bot.setProp('name', name)
                 .then(() => bot.say(`Great! I'll call you ${name}`))
                 .then(() => 'finish');
         }
